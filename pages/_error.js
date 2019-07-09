8d1b6d2c990d43012cc0527cb0a9b35e29c36375
aomkirby123 <aomkirby123@gmail.com>
2019-07-09 18:14:16 +0700

    Update to Next.js 9

--- pages/_error.js
+++ pages/_error.js
@@ -1,14 +1,30 @@
-import { h, Component } from 'preact'
+import { h } from 'preact'
 
-class Error extends Component {
-  static getInitialProps({ res, err }) {
+const Error = ({ statusDetail, statusCode }) => {
+    return (
+        <div style={{color: "#000", background: "#fff", fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif', width: "calc(100vh- 16px)", height: "calc(100vh - 16px)", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+            <div>
+                <h1 style={{display: "inline-block", borderRight: "1px solid rgba(0, 0, 0, 0.3)", margin: "0 20px 0 0", padding: "10px 23px 10px 0px", fontSize: "24px", fontWeight: 500, verticalAlign: "top"}}>
+                    {statusCode}
+                </h1>
+                <div style={{display: "inline-block", textAlign: "left", lineHeight: "49px", height: "49px", verticalAlign: "middle"}}>
+                    <h2 style={{fontSize: "14px", fontWeight: "normal", lineHeight: "inherit", margin: 0, padding: 0}}>
+                        {statusDetail[statusCode] ? statusDetail[statusCode] : "undefined"}
+                    </h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+Error.getInitialProps = async ({ res, err }) => {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null
     const statusDetail = {
         '400': 'Bad Request',
         '401': 'Unauthorized',
         '402': 'Payment Required',
         '403': 'Forbidden',
-        '404': 'This page could not be found',
+        '404': 'This page went missing...',
         '405': 'Method Not Allowed',
         '406': 'Not Acceptable',
         '407': 'Proxy Authentication Required',
@@ -31,25 +47,6 @@ class Error extends Component {
         '505': 'HTTP Version Not Supported',
     };
     return { statusCode, statusDetail }
-  }
-
-  render() {
-    let { statusDetail, statusCode } = this.props
-    return (
-        <div style={{color: "#000", background: "#fff", fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif', width: "calc(100vh- 16px)", height: "calc(100vh - 16px)", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-            <div>
-                <h1 style={{display: "inline-block", borderRight: "1px solid rgba(0, 0, 0, 0.3)", margin: "0 20px 0 0", padding: "10px 23px 10px 0px", fontSize: "24px", fontWeight: 500, verticalAlign: "top"}}>
-                    {statusCode}
-                </h1>
-                <div style={{display: "inline-block", textAlign: "left", lineHeight: "49px", height: "49px", verticalAlign: "middle"}}>
-                    <h2 style={{fontSize: "14px", fontWeight: "normal", lineHeight: "inherit", margin: 0, padding: 0}}>
-                        {statusDetail[statusCode] ? statusDetail[statusCode] : "undefined"}
-                    </h2>
-                </div>
-            </div>
-        </div>
-        )
-    }
 }
 
 export default Error
